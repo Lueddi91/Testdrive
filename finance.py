6364a935e48dcb4712acba042ad0adc9baa2c93e
Daniel <daniel.luethke@gmail.com>
2024-11-23 11:50:48 +0100

    feat: Overview for given input

--- finance.py
+++ finance.py
@@ -4,5 +4,6 @@ ticker = input("Welcher Ticker soll abgerufen werden? \n")
 
 kurs = yf.Ticker(ticker).fast_info
 
-print(kurs.last_price, kurs.currency)
+for k in kurs:
+    print(k, ": ", kurs[k])
 
